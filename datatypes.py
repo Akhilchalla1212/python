03bde605bfc98e3deb2c06d29772178f71cb9ba7
Akhilchalla1212 <akhil.edu1212@gmail.com>
2026-09-18 09:52:37 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,49 @@
+# Integer
+
+a=10
+print(type(a))
+
+# Float
+
+b=3.14
+print(b)
+print(type(b))
+
+# Boolean
+
+phone=True
+print(phone)
+print(type(phone))
+
+
+
+# integer
+
+a=60
+print(a)
+print(type(a))
+
+# float
+
+b=5.6
+print(b)
+print(type(b))
+
+# boolean
+
+phone=False
+print(phone)
+print(type(phone))
+
+#string
+name="ASAN"
+print(name)
+print(type(name))
+#integer
+a=100
+print(a)
+print(type(a))
+#string
+name="goodending"
+print(name)
+print(type(name))
